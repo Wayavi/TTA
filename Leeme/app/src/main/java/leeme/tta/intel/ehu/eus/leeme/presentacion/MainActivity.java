961a76bdb82da29b8624ba069da039a799b72241
Alexander <Alexander>
2016-01-05 09:46:36 +0100

    layouts en horizontal

--- Leeme/app/src/main/java/leeme/tta/intel/ehu/eus/leeme/presentacion/MainActivity.java
+++ Leeme/app/src/main/java/leeme/tta/intel/ehu/eus/leeme/presentacion/MainActivity.java
@@ -1,14 +1,18 @@
 package leeme.tta.intel.ehu.eus.leeme.presentacion;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
+import android.view.Surface;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 import leeme.tta.intel.ehu.eus.leeme.R;
 
@@ -17,7 +21,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Display display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+        if(display.getRotation() == Surface.ROTATION_0)
+            setContentView(R.layout.activity_main);
+        else if(display.getRotation()== Surface.ROTATION_270 || display.getRotation()== Surface.ROTATION_90)
+            setContentView(R.layout.activity_main_horizontal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
--- Leeme/app/src/main/java/leeme/tta/intel/ehu/eus/leeme/presentacion/Menu2Activity.java
+++ Leeme/app/src/main/java/leeme/tta/intel/ehu/eus/leeme/presentacion/Menu2Activity.java
@@ -5,23 +5,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Display;
-import android.view.Surface;
 import android.view.View;
-import android.view.WindowManager;
 
 import leeme.tta.intel.ehu.eus.leeme.R;
 
-public class MenuActivity extends AppCompatActivity {
+public class Menu2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Display display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
-        if(display.getRotation() == Surface.ROTATION_0)
-            setContentView(R.layout.activity_menu);
-        else if(display.getRotation()== Surface.ROTATION_270 || display.getRotation()== Surface.ROTATION_90)
-            setContentView(R.layout.activity_menu_horizontal);
+        setContentView(R.layout.activity_menu2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 

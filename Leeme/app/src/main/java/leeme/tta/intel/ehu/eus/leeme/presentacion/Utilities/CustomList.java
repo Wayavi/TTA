d63a4af0ff7cfadd9d3c14e4c540eaeb2e3e5f19
Markel Bilbao <mabilbao031@ikasle.ehu.eus>
2016-01-15 17:52:01 +0100

    Terminada lista de palabras con imagenes y visualización de video correspondiente al pulsar. Falta aplicar el resize a las imagenes de la lista (la app peta por quedarse sin memoria)

--- Leeme/app/src/main/java/leeme/tta/intel/ehu/eus/leeme/presentacion/Utilities/CustomList.java
+++ Leeme/app/src/main/java/leeme/tta/intel/ehu/eus/leeme/presentacion/Utilities/CustomList.java
@@ -1,5 +1,6 @@
 package leeme.tta.intel.ehu.eus.leeme.presentacion.Utilities;
 
+
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,52 +10,66 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-
 import leeme.tta.intel.ehu.eus.leeme.R;
 
-public class CustomList extends ArrayAdapter<String>{
+public class CustomList extends ArrayAdapter<String>
+{
+    private final String SERVER_URL = "http://51.254.127.111/Leeme";
 
     private final Activity context;
-    private final String[] palabra;
-    private final URL[] imageUrl;
-    private TextView txtTitle;
-    private ImageView imageView;
-    private int pos;
-    private Bitmap myBitmap;
+    private final String[] cadenas;
+    private final String[] urlImagenes;
 
-    public CustomList(Activity context, String[] pal, URL[] imgurl)
+    private ImageView img;
+    private TextView texto;
+
+    public CustomList(Activity context, String[] cadenas, String urls[])
     {
-        super(context, R.layout.listitem, pal);
+        super(context, R.layout.listitem, cadenas);
         this.context = context;
-        this.palabra = pal;
-        this.imageUrl = imgurl;
+        this.cadenas = cadenas;
+        this.urlImagenes = urls;
     }
 
     @Override
     public View getView(final int position, View view, ViewGroup parent)
     {
         LayoutInflater inflater = context.getLayoutInflater();
-        View rowView= inflater.inflate(R.layout.listitem, null, true);
-        txtTitle = (TextView) rowView.findViewById(R.id.txt);
-        imageView = (ImageView) rowView.findViewById(R.id.img);
-        txtTitle.setText(palabra[position]);
+        View listRow = inflater.inflate(R.layout.listitem, null);
+        texto = (TextView)listRow.findViewById(R.id.listitem_txt);
+        texto.setText(cadenas[position]);
+        img = (ImageView)listRow.findViewById(R.id.listitem_img);
+        //img.setImageResource(R.drawable.icon);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String path = SERVER_URL + '/' + urlImagenes[position];
+                try
+                {
+                    URL url = new URL(path);
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    int code = conn.getResponseCode();
+                    if(code == 200)
+                    {
+                        InputStream is = conn.getInputStream();
+                        final Bitmap bitmap = BitmapFactory.decodeStream(is);
+                        //TODO: falta aplicar el resize al bitmap para que la app no pete por falta de memoria en la carga de imagen
+                        img.setImageBitmap(bitmap);
+                    }
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
 
-        pos = position;
-        //imageView.setImageResource(imageId[position]);
-
-        return rowView;
+        return listRow;
     }
-
-
-
-
-
 
 }
